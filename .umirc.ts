2b9db0beac09ea94e87298115327a9fba9c3758f
郑家豪 <beatboxzjh@163.com>
2023-08-13 21:38:56 +0800

    feat:添加github page配置

--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "umi";
 
+const isDev = process.env.NODE_ENV === "development";
+
 export default defineConfig({
   routes: [{ path: "/", component: "index" }],
-
   npmClient: "pnpm",
   tailwindcss: {},
+  hash: true,
+  publicPath: isDev ? "/" : "./",
   plugins: ["@umijs/plugins/dist/tailwindcss"],
 });
