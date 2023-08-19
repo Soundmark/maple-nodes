26acf18502b34f738e18f5e751a3de934c800a56
郑家豪 <beatboxzjh@163.com>
2023-08-20 00:45:53 +0800

    feat:添加百度统计，系统标题和图表

--- .umirc.ts
+++ .umirc.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "umi";
 const isDev = process.env.NODE_ENV === "development";
 
 export default defineConfig({
+  title: "核心计算器",
+  favicons: ["./favicon.ico"],
   routes: [{ path: "/", component: "index" }],
   npmClient: "pnpm",
   tailwindcss: {},
@@ -10,4 +12,17 @@ export default defineConfig({
   history: { type: "hash" },
   publicPath: isDev ? "/" : "./",
   plugins: ["@umijs/plugins/dist/tailwindcss"],
+  headScripts: [
+    {
+      content: `
+  var _hmt = _hmt || [];
+  (function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?8799418b926b0861d0acf752816920d9";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(hm, s);
+  })();
+  `,
+    },
+  ],
 });
